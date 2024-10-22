0ef0ef7f50a5e4971e9121f3388adbc06d062fb0
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-10-21 23:58:51 -0400

    Create Button Class

--- Button.java
+++ Button.java
@@ -1,16 +1,23 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Button here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Button extends Actor
+public class Button extends Actor 
 {
-    
-    public void act()
+    private Runnable action;
+
+    public Button(Runnable action) 
     {
-        // Add your action code here.
+        this.action = action;
+        GreenfootImage image = new GreenfootImage("images/buttonLong_beige.png");
+        setImage(image);
+    }
+
+    public void act() 
+    {
+        if (Greenfoot.mouseClicked(this)) 
+        {
+            if (action != null) {
+                action.run();
+            }
+        }
     }
 }
