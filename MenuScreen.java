a927100ccda0fda23b69402cb4215b6e58993da7
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-10-22 00:55:15 -0400

    Create Avatar

--- MenuScreen.java
+++ MenuScreen.java
@@ -3,11 +3,35 @@ import greenfoot.*;
 
 public class MenuScreen extends World
 {
-    
+    private Queues avatarQueue;
+    private String[] avatars = {"images/face_a.png", "images/face_b.png", "images/face_b.png"};
+    private int currentAvatarIndex = 0;
+
     public MenuScreen()
     {
-         super(600, 400, 1);
-         addObject(new Button(() -> Greenfoot.setWorld(new InstructionScreen())), 300, 340); 
+        super(600, 400, 1);
+
+        avatarQueue = new Queues();
+        for (String avatar : avatars) {
+            avatarQueue.enqueue(avatar);
+        }
+
+        addObject(new Button(this::startInstructions), 300, 340); 
+        addObject(new Button(this::nextAvatar), 400, 350); // Next Avatar Button
+        showAvatar();
     }
 
+    private void startInstructions() {
+        Greenfoot.setWorld(new InstructionScreen());
+    }
+
+    private void nextAvatar() {
+        currentAvatarIndex = (currentAvatarIndex + 1) % avatars.length;
+        showAvatar();
+    }
+
+    private void showAvatar() {
+        removeObjects(getObjects(Avatar.class));
+        addObject(new Avatar(avatars[currentAvatarIndex]), 400, 200);
+    }
 }
